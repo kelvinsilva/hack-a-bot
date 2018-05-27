c6252a46c2450e51fa5667d94f23e181f06cb0b4
kelvin <kelvin@hacklabs.com>
2018-05-27 11:11:07 -0700

    Got servo test working

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+#define SERVO_PIN 5
+
+Servo myservo;  // create servo object to control a servo
+
+void setup() {
+  myservo.attach(SERVO_PIN);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-
-
+//  myservo.write(45);                  // sets the servo position according to the scaled value
+  delay(500);                           // waits for the servo to get there
+//  myservo.write(120);
+  delay(500);
 }
+
