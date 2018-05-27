572eec9d0e5b9591b282f34caab088004ae8b089
Daniel Hunter <officialdanielhunter@gmail.com>
2018-05-27 15:17:29 -0700

    Moved ping sensor lib into main folder

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,18 @@
 #include "motors.h"
+#include "ping.h"
 
 #define BAUD_RATE 9600
 
+// Ping Setup
+#define ECHO_PIN 11
+#define TRIGGER_PIN 12
+Ping ping_sensor;
+
+
 void setup() {
   Serial.begin(BAUD_RATE);
+
+  // Motor Setup
   pinMode(MOTOR_B_DIR, OUTPUT);
   pinMode(MOTOR_B_PWM, OUTPUT);
   digitalWrite(MOTOR_B_DIR, LOW );
@@ -14,11 +23,17 @@ void setup() {
   digitalWrite(MOTOR_A_DIR, LOW);
   digitalWrite(MOTOR_A_PWM, LOW);
 
+  // Ping Setup
+  ping_sensor.begin(TRIGGER_PIN, ECHO_PIN);  
+
 }
 
 
 void loop() {
   // State machine loop goes here
-  motorForwardBack(0);
+  //  motorForwardBack(0);
+  if (ping.distanceFilter()) {
+    // You see a hand, do some stuff
+  }
 }
 
---
+++ main/ping.cpp
@@ -0,0 +1,87 @@
+/*
+  ping.h - Basic ping sensor library - implementation
+  Copyright (c) 2018 UCSC HAckabot Team 1.  All right reserved.
+*/
+
+#include "Arduino.h"
+
+// include this library's description file
+#include "ping.h"
+
+// include description files for other libraries used (if any)
+
+// Public Methods //////////////////////////////////////////////////////////////
+// Functions available in Wiring sketches, this library, and other libraries
+
+void Ping::begin(int trigP, int echoP)
+{
+  // initialize this instance's variables
+  trigPin = trigP;
+  echoPin = echoP;
+
+  // do whatever is required to initialize the library
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+
+  for(int i=0; i<NUM_IN_ROW; i++)
+  {
+    ringBuf[i] = false;
+  }
+
+  index = 0;
+  ringTotal = 0;
+}
+
+int Ping::readDistance(void)
+{
+  long pulseLength;
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  pulseLength = pulseIn(echoPin, HIGH, PING_TIMEOUT_US);
+
+  return (int)(pulseLength / HCSRO4_US_TO_CM_DIVISOR);
+}
+
+bool Ping::distanceFilter(void)
+{
+  int pingDistance = readDistance();
+
+  if(pingDistance > TRUEDISTANCE)
+  {
+    if(ringBuf[index] == true)
+    {
+      ringTotal--;
+    }
+    
+    ringBuf[index] = false;
+  }
+  else
+  {
+    if(ringBuf[index] == false)
+    {
+      ringTotal++;
+    }
+    
+    ringBuf[index] = true;
+  }
+
+  index = (index + 1) % NUM_IN_ROW;
+
+  if(ringTotal == NUM_IN_ROW)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+// Private Methods /////////////////////////////////////////////////////////////
+// Functions only available to other functions in this library
+
