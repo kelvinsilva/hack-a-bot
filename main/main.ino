a909a4d2d46c3a3f67ed2ff46c671b893c5c1a48
Trevor Muraro <trevormuraro@gmail.com>
2018-05-27 14:55:24 -0700

    Beginning of mail loop

--- main/main.ino
+++ main/main.ino
@@ -1,17 +1,61 @@
-#include <Servo.h>
+#include "ping.h"
 
-#define SERVO_PIN 5
+#define FORWARD_SPEED 10
 
-Servo myservo;  // create servo object to control a servo
+#define LEFT_SERVO_HUG_POS 90
+#define RIGHT_SERVO_HUG_POS 90
+#define LEFT_SERVO_RELEASE_POS 0
+#define RIGHT_SERVO_RELEASE_POS 0
+
+static int triggerPin = 12;
+static int echoPin = 11;
+static int motorPwmPin;
+static int leftMotorDirPin;
+static int rightMotorDirPin;
+static int leftServoPin;
+static int rightServoPin;
+
+Ping ping;
+motors motorObj;
+
+Servo leftServo;
+Servo rightServo;
 
 void setup() {
-  myservo.attach(SERVO_PIN);  // attaches the servo on pin 9 to the servo object
+  ping.begin(triggerPin, echoPin);
+  motorObj.motors(motorPwmPin, leftMotorDirPin, rightMotorDirPin);
+  leftServo.attach(leftServoPin);
+  rightServo.attach(rightServoPin);
 }
 
 void loop() {
-//  myservo.write(45);                  // sets the servo position according to the scaled value
-  delay(500);                           // waits for the servo to get there
-//  myservo.write(120);
-  delay(500);
+  //Check for a hand
+
+  if(ping.distanceFilter()) {
+    motorObj.motor_cmd(FORWARD_SPEED);
+    
+    delay(1000);
+
+    motorObj.motor_cmd(0);
+    servo_hug();
+
+    delay(500);
+
+    //Play audio clip
+
+    servo_release();
+  }
+  else {
+    delay(100);
+  }
 }
 
+void servo_hug(void) {
+  leftServo.write(LEFT_SERVO_HUG_POS);
+  rightServo.write(RIGHT_SERVO_HUG_POS);
+}
+
+void servo_release(void) {
+  leftServo.write(LEFT_SERVO_RELEASE_POS);
+  rightServo.write(RIGHT_SERVO_RELEASE_POS);
+}
